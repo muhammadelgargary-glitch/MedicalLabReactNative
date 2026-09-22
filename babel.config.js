c5a0fb967c28a315a74653dfb18dd798f10a52f4
muhammadelgargary-glitch <muhammadelgargary@gmail.com>
2026-09-22 13:05:57 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,22 @@
 module.exports = function(api) {
   api.cache(true);
-  return { presets: ['babel-preset-expo'] };
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': './src',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@store': './src/store',
+            '@utils': './src/utils',
+            '@services': './src/services',
+          },
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
+  };
 };
