cf1463710c2b3f08d09bb48d40e913284917f894
muhammadelgargary-glitch <muhammadelgargary@gmail.com>
2026-09-22 13:18:12 +0300

    Add files via upload

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,4 @@
 module.exports = function(api) {
   api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          alias: {
-            '@': './src',
-            '@components': './src/components',
-            '@screens': './src/screens',
-            '@store': './src/store',
-            '@utils': './src/utils',
-            '@services': './src/services',
-          },
-        },
-      ],
-      'react-native-reanimated/plugin',
-    ],
-  };
+  return { presets: ['babel-preset-expo'] };
 };
